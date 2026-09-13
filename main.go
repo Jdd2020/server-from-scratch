bb7102e910123315b7f015b80395baadfad3fa04
Jdd2020 <dutyjames33@gmail.com>
2026-09-13 11:15:42 -0400

    Adding middleware to track # page hits

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
@@ -12,17 +13,39 @@ const FILE_PATH_ROOT = "."
 const IMAGE_PATH = "/assets"
 const URL_PREFIX = "/app"
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
 
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileServerHits.Add(1)
+	return next
+}
+
+func (cfg *apiConfig) resetMetrics() {
+	cfg.fileServerHits.Store(0)
+}
+
+func (cfg *apiConfig) serveMetrics(w http.ResponseWriter, r *http.Request){
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	resp := "Hits : " +  string(cfg.fileServerHits.Load())
+	w.Write([]byte(resp))
+}
 
 func main() {
 	const URL_ROOT = SERVER_ADDR_ROOT + PORT + URL_PREFIX
 
 	mux := http.NewServeMux()
 
+	cfg := apiConfig{fileServerHits : atomic.Int32{}}
+
 
 	dir := http.Dir(FILE_PATH_ROOT)
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(dir) ))
+	mux.Handle("/app/", cfg.middlewareMetricsInc((http.StripPrefix("/app", http.FileServer(dir)))))
 	mux.HandleFunc("/healthz", healtzHandler)
+	mux.HandleFunc("/metrics", cfg.serveMetrics)
 	
 
 	srv := http.Server{
@@ -41,4 +64,9 @@ func healtzHandler(w http.ResponseWriter, r *http.Request){
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 }
