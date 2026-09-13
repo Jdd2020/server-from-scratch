4f1c8aa69c32e8f4d3127aecc52c96a575fb20c4
Jdd2020 <dutyjames33@gmail.com>
2026-09-12 22:31:39 -0400

    Serving images as well as static html

--- main.go
+++ main.go
@@ -9,13 +9,17 @@ import (
 const PORT = ":8080"
 const SERVER_ADDR_ROOT = "http://localhost"
 const FILE_PATH_ROOT = "."
+const IMAGE_PATH = "/assets/logo.png"
 
 func main() {
+	const URL_ROOT = SERVER_ADDR_ROOT + PORT
+
 	mux := http.NewServeMux()
 
 	/* Serve HTML files from the root directory */
 	directory := http.Dir(FILE_PATH_ROOT)
 	fileServer := http.FileServer(directory)
+
 	mux.Handle("/", fileServer)
 	
 
@@ -26,6 +30,7 @@ func main() {
 		Handler: mux,
 	}
 
-	log.Printf("Server running on %s\n", SERVER_ADDR_ROOT + PORT)
-	srv.ListenAndServe()
+	log.Printf("Server running on %s\n", URL_ROOT)
+	log.Printf("Fun image availabile at %s\n", URL_ROOT + IMAGE_PATH)
+	log.Fatal(srv.ListenAndServe())
 }
